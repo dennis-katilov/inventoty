d29f310fc26650d604ec41e7c1923dd46c20d5d7
katilov <katilov.denis@gmail.com>
2024-04-27 08:21:21 +0300

    start inventory file

---
+++ inventory.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    //an array that checks whether the slot in the inventory is full
+    public bool[] isFull;
+
+}
