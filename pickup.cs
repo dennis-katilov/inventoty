7b631379a9cfadf125d650e09a8fd9c41f562d81
katilov <katilov.denis@gmail.com>
2024-04-27 08:31:17 +0300

    start func

---
+++ pickup.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private Invenyory Invenyory;
+    //item in slot
+    public GameObject slotButton;
+
+    private void Start()
+    {
+        
+    }
+}
